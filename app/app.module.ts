8d8653df5115f82e010a30607180b57db794b75c
Patil <uma.a.patil@capgemini.com>
2017-12-28 13:43:42 +0530

    Router program final

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { ProductListComponent }  from './Product/product-list.component';
 import { StarComponent }  from './Product/Feature/star.component';
-//import { ProductDetailComponent }  from './Product/product-detail.component';
+import { ProductDetailComponent }  from './Product/product-detail.component';
 import { WelcomeComponent }  from './home/welcome.component';
 import {ProductService} from './Product/products.service';
 import { HttpClientModule} from '@angular/common/http';
@@ -14,14 +14,14 @@ import { RouterModule } from '@angular/router';
      imports:[ BrowserModule,FormsModule,HttpClientModule,
         RouterModule.forRoot([
             { path: 'products', component: ProductListComponent},
-            //{ path: 'products/:id', component: ProductDetailComponent},
-            { path: 'welcome', component: WelcomeComponent},
+            { path: 'products/:id', component: ProductDetailComponent},
+            {path: 'welcome', component: WelcomeComponent},
             { path: '', redirectTo: 'welcome', pathMatch: 'full' },
             { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
             ],{ useHash: true }),
             
     ],
-     declarations:[ AppComponent,ProductListComponent,StarComponent,WelcomeComponent], 
+     declarations:[ AppComponent,ProductListComponent,StarComponent,WelcomeComponent,ProductDetailComponent], 
      bootstrap: [ AppComponent ] ,
      providers: [ProductService]
      }
